ade2aba0a44242d073dd29e86bc2067a090424c4
taki <tashfiqahmed196@gmail.com>
2024-09-14 20:06:57 +0600

    clean up

--- emacs.d/transient/history.el
+++
@@ -1,5 +0,0 @@
-((emoji--command-Emoji nil)
- (emoji--command-Emoji\ >\ Animals\ &\ Nature nil)
- (emoji--command-Emoji\ >\ Animals\ &\ Nature\ >\ plant nil)
- (emoji--command-Emoji\ >\ Animals\ &\ Nature\ >\ plant\ >\ other nil)
- (magit-commit nil))
